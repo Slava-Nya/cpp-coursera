11d9e1c055b7a8b3b583df498237991ceac5072f
macbook <svyatoslavank@gmail.com>
2021-01-10 17:15:18 +0300

    3_names1 finished

--- 3_names1/main.cpp
+++ 3_names1/main.cpp
@@ -43,7 +43,7 @@ class Person {
 			}
 			  if (s1 > "" && s2 == "")
 				  return (s1 + " with unknown last name");
-			  if (s2 == "" && s1 > "")
+			  if (s1 == "" && s2 > "")
 				  return (s2 + " with unknown first name");
 			  return ("Incognito");
 	  }
@@ -61,19 +61,22 @@ class Person {
 int main() {
 	Person person;
 
-	person.ChangeFirstName(1965, "Polina");
-	person.ChangeLastName(1967, "Sergeeva");
-	for (int year : {1900, 1965, 1990}) {
+	cout << person.GetFullName(2000) << endl;
+	person.ChangeLastName(1968, "1968_2nd");
+	person.ChangeLastName(1967, "1967_2nd");
+	person.ChangeLastName(1965, "1965_2nd");
+	person.ChangeLastName(1966, "1966_2nd");
+
+	for (int year : {1900, 1920, 1950, 1965, 1966, 1967, 1968}) {
 		cout << person.GetFullName(year) << endl;
 	}
 
-	person.ChangeFirstName(1970, "Appolinaria");
-	for (int year : {1969, 1970}) {
-		cout << person.GetFullName(year) << endl;
-	}
+	person.ChangeFirstName(1920, "1920_1st");
+	person.ChangeFirstName(1900, "1900_1st");
+	person.ChangeFirstName(1965, "1965_1st");
+	person.ChangeFirstName(1950, "1950_1st");
 
-	person.ChangeLastName(1968, "Volkova");
-	for (int year : {1969, 1970}) {
+	for (int year : {1900, 1920, 1950, 1965, 1966, 1967, 1968}) {
 		cout << person.GetFullName(year) << endl;
 	}
 
