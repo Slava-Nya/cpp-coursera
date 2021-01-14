671e2c27ebd6891b0f8608b3addd5b62406a302e
macbook <svyatoslavank@gmail.com>
2021-01-14 17:09:04 +0300

    4_invertible_function finished

--- 3_names3/main.cpp
+++ 3_names3/main.cpp
@@ -136,7 +136,7 @@ class Person {
 //	for (int year : {1959, 1960}) {
 //		cout << person.GetFullNameWithHistory(year) << endl;
 //	}
-//˚µ
+//
 //	person.ChangeFirstName(1965, "Appolinaria");
 //	person.ChangeLastName(1967, "Ivanova");
 //	for (int year : {1965, 1967}) {
