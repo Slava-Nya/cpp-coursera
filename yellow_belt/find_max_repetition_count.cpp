011bf7773467d13979a9f8198a8b070216c80e3a
macbook <svyatoslavank@gmail.com>
2021-03-01 19:26:53 +0300

    find_max_repetition_count.cpp

--- yellow_belt/find_max_repetition_count.cpp
+++ yellow_belt/find_max_repetition_count.cpp
@@ -3,9 +3,16 @@
 //
 
 #include <tuple>
-
+#include <map>
+#include <vector>
+#include <string>
+#include <iostream>
 
 using namespace std;
+
+enum class Lang {
+	DE, FR, IT
+};
 
 struct Region {
 	string std_name;
@@ -14,9 +21,18 @@ struct Region {
 	int64_t population;
 };
 
-int FindMaxRepetitionCount(const vector<Region>& regions)
-{
+bool operator<(const Region& lhs, const Region& rhs) {
+	return tie(lhs.std_name, lhs.parent_std_name, lhs.names, lhs.population) <
+		   tie(rhs.std_name, rhs.parent_std_name, rhs.names, rhs.population);
+}
 
+int FindMaxRepetitionCount(const vector<Region>& regions) {
+	int result = 0;
+	map<Region, int> repetion_count;
+	for (const Region& region : regions) {
+		result = max(result, ++repetion_count[region]);
+	}
+	return result;
 }
 
 int main() {
@@ -25,6 +41,11 @@ int main() {
 		   "Moscow",
 		   "Russia",
 		   {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
+		   89
+   },{
+		   "Moscow",
+		   "Russia",
+		   {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Russia"}},
 		   89
    }, {
 		   "Russia",
